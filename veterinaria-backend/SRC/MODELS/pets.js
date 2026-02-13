fa9ea796896cc1050050636994de5b256de2009a
JuanRdz05 <juanrdz140505@gmail.com>
2026-02-12 20:43:42 -0600

    Modifique el modelo de las mascotas

--- veterinaria-backend/SRC/MODELS/pets.js
+++ veterinaria-backend/SRC/MODELS/pets.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import validateBreed from "../LIBS/models/breedValidator.js";
 
 const petSchema = new mongoose.Schema(
 	{
@@ -11,43 +10,19 @@ const petSchema = new mongoose.Schema(
 		name: {
 			type: String,
 			required: true,
+			trim: true, // Elimina espacios en blanco accidentales
 		},
 		birthday: {
 			type: Date,
 			required: true,
 		},
-		speciesCategory: {
+		// Consolidamos todo en un solo campo descriptivo
+		petType: {
 			type: String,
 			required: true,
-			enum: ["mammal", "bird", "reptile", "fish", "other"],
-		},
-		species: {
-			type: String,
-			required: true,
-			enum: [
-				"dog",
-				"cat",
-				"rabbit",
-				"hamster",
-				"guinea pig",
-				"ferret",
-				"parrot",
-				"canary",
-				"cockatiel",
-				"parakeet",
-				"turtle",
-				"lizard",
-				"snake",
-				"iguana",
-				"goldfish",
-				"betta",
-				"tropical",
-				"other",
-			],
-			validate: {
-				validator: validateBreed,
-				message: (props) =>
-					`Species "${props.value}" is not valid for category "${props.instance.speciesCategory}"`,
+			enum: {
+				values: ["perro", "gato", "conejo", "ave", "reptil", "pez", "otro"],
+				message: "{VALUE} no es un tipo de mascota soportado",
 			},
 		},
 		weight: {
