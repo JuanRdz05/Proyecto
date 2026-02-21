afd8c71010453735964d569ef922a9aa3e7b4a56
JuanRdz05 <juanrdz140505@gmail.com>
2026-02-20 22:37:18 -0600

    Modifique el middleware para que verifique los simbolos especiales ademas de los números

--- veterinaria-backend/SRC/MIDDLEWARES/noNumbers.js
+++ veterinaria-backend/SRC/MIDDLEWARES/noNumbers.js
@@ -3,9 +3,9 @@ const noNumbers = (fields) => {
 	return (req, res, next) => {
 		for (const field of fields) {
 			const value = req.body[field];
-			if (value && /\d/.test(value)) {
+			if (value && /[0-9!@#$%^&*()_+={}\[\]|\\:;"'<>,.?/~]/.test(value)) {
 				return res.status(400).json({
-					message: `El campo ${field} no puede contener números`,
+					message: `El campo ${field} no puede contener números ni simbolos especiales`,
 				});
 			}
 		}
