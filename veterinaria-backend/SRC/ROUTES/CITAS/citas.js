25f40e155344c338a24545f06eba5712bc041c55
JuanRdz05 <juanrdz140505@gmail.com>
2026-03-07 22:33:40 -0600

    Función para cancelar citas implementada.

--- veterinaria-backend/SRC/ROUTES/CITAS/citas.js
+++ veterinaria-backend/SRC/ROUTES/CITAS/citas.js
@@ -2,6 +2,8 @@ const citasRouter = require("express").Router();
 const {
 	createAppointments,
 	getAllAppointments,
+	getAppointmentsByUser,
+	cancelAppointment,
 } = require("../../CONTROLLERS/CITAS/citas.js");
 const { verificarToken } = require("../../MIDDLEWARES/authToken.js");
 
@@ -9,5 +11,9 @@ const { verificarToken } = require("../../MIDDLEWARES/authToken.js");
 citasRouter.post("/create", verificarToken, createAppointments);
 //Rutas para obtener todas las citas en general
 citasRouter.get("/all", verificarToken, getAllAppointments);
+//Rutas para obtener las citas por usuario
+citasRouter.get("/user/:id", verificarToken, getAppointmentsByUser);
+//Ruta para cancelar una cita
+citasRouter.delete("/:id", verificarToken, cancelAppointment);
 
 module.exports = citasRouter;
